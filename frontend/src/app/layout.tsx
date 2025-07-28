ca051ff60d5e602b7b62d6b146dca0071d99fcf9
MikleSib <Mishatrof21@gmail.com>
2025-07-28 15:35:20 +0700

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
@@ -17,6 +18,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103508060', 'ym');
+
+              ym(103508060, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103508060" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
