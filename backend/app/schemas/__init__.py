cb90afa07c411654eee9fd67dbed37554b7cb5d4
MikleSib <Mishatrof21@gmail.com>
2025-07-31 01:36:02 +0700

    Update __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,10 +1,10 @@
-from .user import User, UserCreate, UserUpdate, UserInDB, Token, TokenPayload
+from .user import User, UserCreate, UserUpdate, UserInDB, Token, TokenPayload, EmailVerificationCode, EmailVerificationRequest
 from .streamer import Streamer, StreamerCreate, StreamerUpdate, StreamerWithUser
 from .donation import Donation, DonationCreate, DonationUpdate, DonationWithDetails, DonationStats
 from .alert_settings import AlertSettings, AlertSettingsCreate, AlertSettingsUpdate, AlertSettingsInDB
 
 __all__ = [
-    "User", "UserCreate", "UserUpdate", "UserInDB", "Token", "TokenPayload",
+    "User", "UserCreate", "UserUpdate", "UserInDB", "Token", "TokenPayload", "EmailVerificationCode", "EmailVerificationRequest",
     "Streamer", "StreamerCreate", "StreamerUpdate", "StreamerWithUser", 
     "Donation", "DonationCreate", "DonationUpdate", "DonationWithDetails", "DonationStats",
     "AlertSettings", "AlertSettingsCreate", "AlertSettingsUpdate", "AlertSettingsInDB"
